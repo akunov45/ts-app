972a5e4f9928b2313bdb4110f5d9cd4b07651cef
Kutman Akunov <kutman@mancho.dev>
2021-11-10 02:01:52 +0600

    first commit

---
+++ app/test.js
@@ -0,0 +1,27 @@
+let salam = 'Test'
+console.log(salam);
+
+let btn = document.getElementById('btn')
+btn.addEventListener('click', (e) => {
+    console.log(e)
+})
+alert("")
+
+if (true) {
+    for (const key in object) {
+        if (Object.hasOwnProperty.call(object, key)) {
+            const element = object[key];
+
+        }
+    }
+}
+//nfn
+const Add = (params) => {
+    console.log()
+
+}
+console.log('dsdd')
+
+const btn = document.querySelector('btn')
+
+const app = document.querySelector('app')
